4cbe3740a9c5d461f134b2d87e7eafb43e6cbf3b
Tushar Sharma <tstusharsharma9292@gmail.com>
2026-08-06 19:21:05 +0530

    Variables and Datatypes

---
+++ variables_and_datatype.cpp
@@ -0,0 +1,75 @@
+//AREA OF SQUARE
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int side;
+//     cout<<"enter the side of the square ";
+//     cin>>side;
+//     cout<<"the area of the square is: "<<side*side<<endl;
+
+// return 0;
+// }
+
+
+//GST CALCULATOR
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     float pencost, pencilcost, erasercost;
+//     cout<<"enter the cost of pen: ";
+//     cin>>pencost;
+//     cout<<"enter the cost of pencil: ";
+//     cin>>pencilcost;
+//     cout<<"enter the cost of eraser: ";
+//     cin>>erasercost;
+
+//     float totalcost = pencost + pencilcost + erasercost;
+//     cout<<"total cost is: "<<totalcost<<endl;
+
+//     cout<<"tota bill after tax ia: "<<(totalcost + 0.18*totalcost)<<endl;
+
+// return 0;
+// }
+
+
+
+//SI CALCULATOR
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     float p , r , t;
+//     cout<<"Enter the amount: ";
+//     cin>>p;
+//     cout<<"Enter the rate of intrest: ";
+//     cin>>r;
+//     cout<<"Enter the time duration: ";
+//     cin>>t;
+    
+//     float SI= (p*r*t)/100;
+//     cout<<"the simple intrest is: "<<SI<<endl;
+
+// return 0;
+// }
+
+
+
+// //AREA OF CIRCLE
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int r;
+//     cout<<"enter the radius: ";
+//     cin>>r;
+//     cout<<"the arae of the squarer is: "<<3.14*r*r<<endl;
+
+// return 0;
+// }
