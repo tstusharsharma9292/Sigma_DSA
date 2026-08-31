4292a8491d64bf224efb6d4ff8bde0a841f06c99
Tushar Sharma <tstusharsharma9292@gmail.com>
2026-08-31 21:15:27 +0530

    Practice

--- operators.cpp
+++ operators.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hellow world";
-return 0;
-}
+// Question 1
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int x = 2 , y = 5;
+// int exp1 = (x * y / x);
+// int exp2 = (x * ( y / x));
+// cout << exp1 <<"\n" ;
+// cout << exp2 <<"\n";
+
+// return 0;
+// }
+
+
+// Question 2
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x = 10 , y = 5;
+//     int exp1 = (y * (x / y + x / y));
+//     int exp2 = (y * x / y + y * x / y);
+
+//     cout << exp1 <<"\n";
+//     cout << exp2 <<"\n";
+// return 0;
+// }
